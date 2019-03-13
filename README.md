0162b42d89a57afa42c595bc2f5fa983e46c0b45
KSticc <k.allen19133@gmail.com>
2019-03-13 15:04:30 -0700

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Things
+
+Random python things. May or may not reformat your hard drive
---
+++ morsepi/morsepi.py
@@ -0,0 +1,60 @@
+from gpiozero import LED
+from time import sleep
+import string as string
+led = LED(18)
+
+MORSE_DICT = {'A':'.-', 'B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.','G':'--.',
+'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..','M':'--','N':'-.','O':'---',
+'P':'.--.','Q':'--.-','R':'.-.','S':'...','T':'-','U':'..-','V':'...-','W':'.--',
+'X':'-..-','Y':'-.--','Z':'--..','1':'.----','2':'..---','3':'...--','4':'....-',
+'5':'.....','6':'-....','7':'--...','8':'---..','9':'----.','0':'-----',',':'--..--',
+'.':'.-.-.-','?':'..--..','/':'-..-.','-':'-....-','(':'-.--.',')':'-.--..-'}
+
+def setLED(pin):
+	led = LED(pin)
+
+def _dot():
+	led.on()
+	sleep(0.5)
+	led.off()
+	sleep(0.15)
+
+def _dash():
+	led.on()
+	sleep(1.5)
+	led.off()
+	sleep(0.15)
+
+def _space():
+	led.off()
+	sleep(1.5)
+
+def _pause():
+	led.off()
+	sleep(0.5)
+
+def morseEncrypt(string):
+	string = string.upper()
+	cipher = ' '
+	for letter in string:
+		if letter != ' ':
+			cipher += MORSE_DICT[letter] + ' '
+		else:
+			cipher += ' '
+	return cipher
+
+def morseSpeak(string):
+	encoded_string = morseEncrypt(string)
+	for character in encoded_string:
+		if character == '-':
+			_dash()
+		elif character == '.':
+			_dot()
+		elif character == ' ':
+			_pause()
+		else:
+			_space()
+
+
+sleep(0.5)
+morseSpeak('hi')
