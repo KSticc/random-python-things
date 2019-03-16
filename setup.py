348a35c4aeadf512572a5e0e0c8cc5e44f60285e
KSting <k.allen19133@gmail.com>
2019-03-15 18:40:59 -0700

    Added more info

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 __project__ = "morsepi"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __description__ = "LED morse code for the Raspberry Pi"
 __packages__ = ["morsepi"]
+__author__ = "KSting"
 
 setup(
 	name = __project__,
 	version = __version__,
 	description = __description__,
 	packages = __packages__,
+	author = __author__,
 )
